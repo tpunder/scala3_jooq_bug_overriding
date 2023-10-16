023c78d9267a8e30d11c04cc871f95a4feb7f257
Tim Underwood <timunderwood@gmail.com>
2023-10-16 13:50:34 -0700

    Slight simplification for example

--- src/main/java/com/example/ExampleRecordJava.java
+++ src/main/java/com/example/ExampleRecordJava.java
@@ -1,9 +1,8 @@
 package com.example;
 
-import org.jooq.Record;
 import org.jooq.impl.TableRecordImpl;
 
 // This works fine
-class ExampleRecordJava extends TableRecordImpl<ExampleRecordJava> implements Record {
+class ExampleRecordJava extends TableRecordImpl<ExampleRecordJava> {
 
 }
--- src/main/scala/com/example/ExampleRecordScala.scala
+++ src/main/scala/com/example/ExampleRecordScala.scala
@@ -1,9 +1,8 @@
 package com.example
 
-import org.jooq.Record
 import org.jooq.impl.TableRecordImpl
 
 // Works in Scala 2.12 and Scala 2.13 but is broken in Scala 3
-class ExampleRecordScala extends TableRecordImpl[ExampleRecordScala] with Record {
+class ExampleRecordScala extends TableRecordImpl[ExampleRecordScala] {
 
 }
